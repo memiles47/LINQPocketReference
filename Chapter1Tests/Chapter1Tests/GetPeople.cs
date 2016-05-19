43a914b258192eb2090bc438e38e6855d4cdc30c
Michael Miles <boat081062@gmail.com>
2016-05-19 07:13:14 -0500

    Added Greg Logue

--- Chapter1Tests/Chapter1Tests/GetPeople.cs
+++ Chapter1Tests/Chapter1Tests/GetPeople.cs
@@ -13,6 +13,7 @@ namespace Chapter1Tests
                 new Family() {FirstsName = "Virginia", LastName = "Miles", BirthDate = new DateTime(1928, 4, 9)},
                 new Family() {FirstsName = "Rick", LastName = "Miles", BirthDate = new DateTime(1947, 6, 7)},
                 new Family() {FirstsName = "Chris", LastName = "Logue", BirthDate = new DateTime(1950, 9, 30)},
+                new Family() {FirstsName = "Greg", LastName = "Logue", BirthDate = new DateTime(1951, 6, 28)},
                 new Family() {FirstsName = "Tyra", LastName = "Wingar", BirthDate = new DateTime(1952, 10, 21)},
                 new Family() {FirstsName = "Mike", LastName = "Miles", BirthDate = new DateTime(1962, 8, 10)},
                 new Family() {FirstsName = "Diane", LastName = "Waller", BirthDate = new DateTime(1964, 6, 12)},

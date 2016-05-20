b801a996a5745c27884de62c48e6aa7d2bb07484
Michael Miles <boat081062@gmail.com>
2016-05-20 10:54:55 -0500

    Taking baby steps adding a database to my LINQ queries

--- Chapter1Tests/Chapter1Tests/DataBase.cs
+++ Chapter1Tests/Chapter1Tests/DataBase.cs
@@ -1,7 +1,18 @@
+using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+
 namespace Chapter1Tests
 {
     public class DataBase
     {
-        
+        [Table(Name = "myDBase")]
+        class Dbase
+        {
+            [Column] public string Id;
+            [Column] public string Name;
+            [Column] public string Title;
+        }
     }
 }
